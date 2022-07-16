02a0f45e14f181ee2470db144418e2e4fb100470
Khdave02 <87975841+Khdave02@users.noreply.github.com>
2022-07-16 10:29:42 +0530

    Update BT20CSE031_Book_Store_Managment_LL.c

--- BT20CSE031_Book_Store_Managment_LL.c
+++ BT20CSE031_Book_Store_Managment_LL.c
@@ -12,132 +12,132 @@ Data Structure Used - Linked List
 #include <string.h>
 
 
-struct Date
-{
-    int yyyy;
-    int mm;
-    int dd;
-};
-typedef struct Date Date;
+// struct Date
+// {
+//     int yyyy;
+//     int mm;
+//     int dd;
+// };
+// typedef struct Date Date;
 
 
-int isLeap(int year) // year is leap or common
-{
-    if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-        return 1;
-    else
-        return 0;
-}
+// int isLeap(int year) // year is leap or common
+// {
+//     if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+//         return 1;
+//     else
+//         return 0;
+// }
 
 
-int offsetDays(int d, int m, int y)
-{
-    int offset = d;
+// int offsetDays(int d, int m, int y)
+// {
+//     int offset = d;
 
-    switch (m - 1)
-    {
-    case 11:
-        offset += 30;
-    case 10:
-        offset += 31;
-    case 9:
-        offset += 30;
-    case 8:
-        offset += 31;
-    case 7:
-        offset += 31;
-    case 6:
-        offset += 30;
-    case 5:
-        offset += 31;
-    case 4:
-        offset += 30;
-    case 3:
-        offset += 31;
-    case 2:
-        offset += 28;
-    case 1:
-        offset += 31;
-    }
+//     switch (m - 1)
+//     {
+//     case 11:
+//         offset += 30;
+//     case 10:
+//         offset += 31;
+//     case 9:
+//         offset += 30;
+//     case 8:
+//         offset += 31;
+//     case 7:
+//         offset += 31;
+//     case 6:
+//         offset += 30;
+//     case 5:
+//         offset += 31;
+//     case 4:
+//         offset += 30;
+//     case 3:
+//         offset += 31;
+//     case 2:
+//         offset += 28;
+//     case 1:
+//         offset += 31;
+//     }
 
-    if (isLeap(y) && m > 2)
-        offset += 1;
+//     if (isLeap(y) && m > 2)
+//         offset += 1;
 
-    return offset;
-}
-
-
-// Given a year and days elapsed in it, finds
-// date by storing results in d and m.
-void revoffsetDays(int offset, int y, int *d, int *m)
-{
-    int month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-    if (isLeap(y))
-        month[2] = 29;
-
-    int i;
-    for (i = 1; i <= 12; i++)
-    {
-        if (offset <= month[i])
-            break;
-        offset = offset - month[i];
-    }
-
-    *d = offset;
-    *m = i;
-}
+//     return offset;
+// }
 
 
-// Add x days to the given date.
-void addDays(Date *d, int x)
-{
-    if (x == -1 && d->dd == 1 && d->mm == 1)
-    {
-        d->dd = 31;
-        d->mm = 12;
-        d->yyyy -= 1;
-        return;
-    }
-    int offset1 = offsetDays(d->dd, d->mm, d->yyyy);
-    int remDays = isLeap(d->yyyy) ? (366 - offset1) : (365 - offset1);
+// // Given a year and days elapsed in it, finds
+// // date by storing results in d and m.
+// void revoffsetDays(int offset, int y, int *d, int *m)
+// {
+//     int month[13] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-    // y2 is going to store result year and
-    // offset2 is going to store offset days
-    // in result year.
-    int y2, offset2;
-    if (x <= remDays)
-    {
-        y2 = d->yyyy;
-        offset2 = offset1 + x;
-    }
+//     if (isLeap(y))
+//         month[2] = 29;
 
-    else
-    {
-        // x may store thousands of days.
-        // We find correct year and offset
-        // in the year.
-        remDays = x - remDays;
-        y2 = d->yyyy + 1;
-        int y2days = isLeap(y2) ? 366 : 365;
-        while (remDays >= y2days)
-        {
-            y2days = x - y2days;
-            y2++;
-            y2days = isLeap(y2) ? 366 : 365;
-        }
-        offset2 = x;
-    }
+//     int i;
+//     for (i = 1; i <= 12; i++)
+//     {
+//         if (offset <= month[i])
+//             break;
+//         offset = offset - month[i];
+//     }
 
-    // Find values of day and month from
-    // offset of result year.
-    int m2, d2;
-    revoffsetDays(offset2, y2, &d2, &m2);
+//     *d = offset;
+//     *m = i;
+// }
 
-    d->yyyy = y2;
-    d->dd = d2;
-    d->mm = m2;
-}
+
+// // Add x days to the given date.
+// void addDays(Date *d, int x)
+// {
+//     if (x == -1 && d->dd == 1 && d->mm == 1)
+//     {
+//         d->dd = 31;
+//         d->mm = 12;
+//         d->yyyy -= 1;
+//         return;
+//     }
+//     int offset1 = offsetDays(d->dd, d->mm, d->yyyy);
+//     int remDays = isLeap(d->yyyy) ? (366 - offset1) : (365 - offset1);
+
+//     // y2 is going to store result year and
+//     // offset2 is going to store offset days
+//     // in result year.
+//     int y2, offset2;
+//     if (x <= remDays)
+//     {
+//         y2 = d->yyyy;
+//         offset2 = offset1 + x;
+//     }
+
+//     else
+//     {
+//         // x may store thousands of days.
+//         // We find correct year and offset
+//         // in the year.
+//         remDays = x - remDays;
+//         y2 = d->yyyy + 1;
+//         int y2days = isLeap(y2) ? 366 : 365;
+//         while (remDays >= y2days)
+//         {
+//             y2days = x - y2days;
+//             y2++;
+//             y2days = isLeap(y2) ? 366 : 365;
+//         }
+//         offset2 = x;
+//     }
+
+//     // Find values of day and month from
+//     // offset of result year.
+//     int m2, d2;
+//     revoffsetDays(offset2, y2, &d2, &m2);
+
+//     d->yyyy = y2;
+//     d->dd = d2;
+//     d->mm = m2;
+// }
 
 
 /*
